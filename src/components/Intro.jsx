9df6ba08670b4e2d153551fad6a4a5cbe5df11ef
Jeremy Kenney <jeremykenney.001@gmail.com>
2024-03-15 14:24:12 -0400

    initial commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Intro() {
+   return (
+      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Jeremy Kenney</h1>
+         <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
+         <p className="text-sm max-w-xl mb-6 font-bold">
+         My aspiration to become a backend developer stems from a deep-seated fascination with the inner workings of technology and a desire to play a pivotal role in shaping digital experiences. For me, backend development represents the foundation upon which the dynamic and interactive elements of websites and applications are built. It's about architecting systems, optimizing databases, and crafting efficient algorithms to ensure seamless functionality.
+<br/>
+I am drawn to the intricacies of server-side programming languages and the challenge of creating robust solutions that withstand the test of time. There's a thrill in unraveling complex problems, optimizing code for performance, and implementing secure data storage mechanisms. Each line of code feels like a puzzle piece, contributing to the larger picture of a functional and user-centric digital ecosystem.
+<br/>
+Being a backend developer isn't just about writing code; it's about crafting experiences. It's about enabling seamless interactions, safeguarding data integrity, and empowering users with responsive and reliable platforms. Through backend development, I aim to channel my creativity and problem-solving skills into building impactful solutions that enhance the way people engage with technology.
+<br/>
+Ultimately, my journey towards becoming a backend developer is driven by a genuine passion for technology and a commitment to leveraging my skills to make a meaningful contribution to the digital realm. It's a journey of continuous learning, exploration, and innovation, and I am excited to embark on this path towards realizing my full potential in the world of backend development.
+         </p>
+      </div>
+   )
+}
+
+export default Intro;
---
+++ src/data/timeline.js
@@ -0,0 +1,30 @@
+export default [
+   {
+		year: 'Today',
+		title: 'Bachelor of Science in Software Engineering [Penn State University]',
+		duration: '4 years',
+		details:
+			"Courses cover a wide array of software engineering topics, including discrete mathematics, probability, and statistics, along with relevant topics in computer science and supporting disciplines for a comprehensive look at modern computer software and techniques.",
+	},
+	{
+		year: '2024',
+		title: 'Artificial Intelligence and Machine Learning [eCornell]',
+		duration: '1 year',
+		details:
+			'Using python, I implemented k-nearest neighbors, naive Bayes, regression trees, and a variety of machine learning algorithms with practice selecting the best model, considering key principles of how to implement those models effectively. I had the opportunity to implement algorithms on live data while practicing debugging and improving models through approaches such as ensemble methods and support vector machines, as well as the inner workings of neural networks and how to construct and adapt neural networks for various types of data.',
+	},
+	{
+		year: '2023',
+		title: "Software Engineer [Coding Temple]",
+		duration: '1 year',
+		details:
+			'I learned about computer architecture, computer science theory, and an array of programming languages.',
+	},
+	{
+		year: '2023',
+		title: 'First Line of Code',
+		duration: 'the beginning',
+		details:
+			"I wrote my first line of real code that started this journey into digital craftsmanship that I never could've imagined, especially as someone who never saw themselves as anything other than a blue collar worker.",
+	},
+]
